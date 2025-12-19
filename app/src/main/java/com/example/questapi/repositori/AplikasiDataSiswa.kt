95f719f590ea84ec7c7a5fe55ba6d13efed76728
galang48 <galangyudha1945@gmail.com>
2025-12-19 10:09:09 +0700

    Menambahkan AplikasiDataSiswa dan mengizinkan cleartext traffic

---
+++ app/src/main/java/com/example/questapi/repositori/AplikasiDataSiswa.kt
@@ -0,0 +1,9 @@
+package com.example.questapi.repositori
+
+import android.app.Application
+
+class AplikasiDataSiswa : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
