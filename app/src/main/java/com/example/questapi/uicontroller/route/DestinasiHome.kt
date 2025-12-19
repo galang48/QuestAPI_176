82a08b41676f8dab042cdb027e31e6b67856a1ab
galang48 <galangyudha1945@gmail.com>
2025-12-19 14:14:53 +0700

    Menambahkan DestinasiHome sebagai DestinasiNavigasi

---
+++ app/src/main/java/com/example/questapi/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.questapi.uicontroller.route
+
+import com.example.questapi.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
