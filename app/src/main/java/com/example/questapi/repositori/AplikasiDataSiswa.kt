cfbb24304af3d0dcf06cba81475800f842fe0ff1
galang48 <galangyudha1945@gmail.com>
2025-12-19 14:45:12 +0700

    Menambahkan ProviderViewModel dan CreationExtrasExt

--- app/src/main/java/com/example/questapi/repositori/AplikasiDataSiswa.kt
+++ app/src/main/java/com/example/questapi/repositori/AplikasiDataSiswa.kt
@@ -1,9 +1,13 @@
 package com.example.questapi.repositori
 
 import android.app.Application
+import com.example.questapi.repositori.container.ContainerApp
+import com.example.questapi.repositori.container.DefaultContainerApp
 
 class AplikasiDataSiswa : Application() {
+    lateinit var container : ContainerApp
     override fun onCreate() {
         super.onCreate()
+        this.container = DefaultContainerApp()
     }
 }
---
+++ app/src/main/java/com/example/questapi/viewmodel/provider/ProviderViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.questapi.viewmodel.provider
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.questapi.repositori.AplikasiDataSiswa
+import com.example.questapi.viewmodel.EntryViewModel
+import com.example.questapi.viewmodel.HomeViewModel
+
+
+object ProviderViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(
+                aplikasiDataSiswa().container
+                    .repositoryDataSiswa
+            )
+        }
+        initializer {
+            EntryViewModel(
+                aplikasiDataSiswa().container
+                    .repositoryDataSiswa
+            )
+        }
+    }
+}
