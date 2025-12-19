883f9faef9ed81ceb6a98d7f22f06cf9c1beca6b
galang48 <galangyudha1945@gmail.com>
2025-12-19 13:24:09 +0700

    Menambahkan kelas data DataSiswa

---
+++ app/src/main/java/com/example/questapi/model/data/DataSiswa.kt
@@ -0,0 +1,11 @@
+package com.example.questapi.model.data
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DataSiswa(
+    val id : Int,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
+)
