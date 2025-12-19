83a1c17f10aa525e2f79ccbeee24af957b7d8028
galang48 <galangyudha1945@gmail.com>
2025-12-19 13:29:30 +0700

    Menambahkan fungsi ekstensi toDataSiswa

---
+++ app/src/main/java/com/example/questapi/model/data/toDataSiswa.kt
@@ -0,0 +1,11 @@
+package com.example.questapi.model.data
+
+import com.example.questapi.model.data.DataSiswa
+import com.example.questapi.model.data.DetailSiswa
+
+fun DetailSiswa.toDataSiswa(): DataSiswa = DataSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
