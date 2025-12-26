d3e861cd860027e406fd9a6016972fa93d71ef3e
galang48 <galangyudha1945@gmail.com>
2025-12-26 17:41:26 +0700

    Menambahkan fungsi getSatuSiswa di DetailViewModel

--- app/src/main/java/com/example/questapi/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi/viewmodel/DetailViewModel.kt
@@ -7,9 +7,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.questapi.model.data.DataSiswa
 import com.example.questapi.repositori.RepositoryDataSiswa
 import com.example.questapi.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import java.io.IOException
+import retrofit2.HttpException
 
 sealed interface StatusUIDetail {
     data class Success(
@@ -29,3 +33,20 @@ class DetailViewModel(
     var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
         private set
 
+    init {
+        getSatuSiswa()
+    }
+
+    fun getSatuSiswa() {
+        viewModelScope.launch {
+            statusUIDetail = StatusUIDetail.Loading
+            statusUIDetail =
+                try {
+                    StatusUIDetail.Success(satuSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa))
+                } catch (e: IOException) {
+                    StatusUIDetail.Error
+                } catch (e: HttpException) {
+                    StatusUIDetail.Error
+                }
+        }
+    }
