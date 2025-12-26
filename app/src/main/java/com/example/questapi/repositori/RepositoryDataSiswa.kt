69110d3e4c61a24b97ae42f83e37b26ca33345f8
galang48 <galangyudha1945@gmail.com>
2025-12-26 14:48:27 +0700

    Mengaktifkan fungsi editSatuSiswa di RepositoryDataSiswa

--- app/src/main/java/com/example/questapi/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi/repositori/RepositoryDataSiswa.kt
@@ -6,6 +6,6 @@ interface RepositoryDataSiswa {
     suspend fun getDataSiswa() : List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa) : retrofit2.Response<Void>
     suspend fun getSatuSiswa(id:Int) : DataSiswa
-//    suspend fun editSatuSiswa(id:Int, dataSiswa: DataSiswa) : retrofit2.Response<Void>
+    suspend fun editSatuSiswa(id:Int, dataSiswa: DataSiswa) : retrofit2.Response<Void>
 //    suspend fun hapusSatuSiswa(id:Int):retrofit2.Response<Void>
 }
