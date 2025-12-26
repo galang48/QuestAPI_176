30e6e57c3ca5b1b2e76f5419f1de1011fcdd64d7
galang48 <galangyudha1945@gmail.com>
2025-12-26 15:05:26 +0700

    Menambahkan fungsi getSatuSiswa di NetworkRepositoryDataSiswa

--- app/src/main/java/com/example/questapi/repositori/NetworkRepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi/repositori/NetworkRepositoryDataSiswa.kt
@@ -8,4 +8,5 @@ class NetworkRepositoryDataSiswa(
 ):RepositoryDataSiswa{
     override suspend fun getDataSiswa() : List<DataSiswa> = serviceApiSiswa.getSiswa()
     override suspend fun postDataSiswa(dataSiswa : DataSiswa):retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
 }
