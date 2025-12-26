10ddd2a47192cf82a9e470e5924aabbb1a5b3521
galang48 <galangyudha1945@gmail.com>
2025-12-26 17:39:15 +0700

    Menambahkan DetailViewModel

--- app/src/main/java/com/example/questapi/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi/viewmodel/DetailViewModel.kt
@@ -2,7 +2,14 @@
 
 package com.example.questapi.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
 import com.example.questapi.model.data.DataSiswa
+import com.example.questapi.repositori.RepositoryDataSiswa
+import com.example.questapi.uicontroller.route.DestinasiDetail
 
 sealed interface StatusUIDetail {
     data class Success(
@@ -13,3 +20,12 @@ sealed interface StatusUIDetail {
 
     object Loading : StatusUIDetail
 }
+
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDataSiswa: RepositoryDataSiswa,
+) : ViewModel() {
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
